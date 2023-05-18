f9629e6ebfe841af31e48bcfe828634551d44b3a
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-05-18 18:20:11 -0300

    Add files via upload

---
+++ src/components/AnimatedText.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {motion} from "framer-motion"
+
+const quote = {
+    initial:{
+        opacity:0,
+    },
+    animate:{
+        opacity:1,
+        transition:{
+            delay:0.5
+        }
+    }
+}
+
+const singleWord = {
+    initial:{
+        opacity:0,
+        y:50,
+    },
+    animate:{
+        opacity:1,
+        y:0,
+        transition:{
+            duration:1
+        }
+    }
+}
+
+const AnimatedText = ({text, className=""}) => {
+    return (
+        <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hiddens">
+            
+            <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-5xl ${className}`} variants={quote} initial="initial" animate="animate">
+                {
+                    text.split(" ").map((word, index)=>
+                    <motion.span key={word+'-'+index} className="inline-block" variants={singleWord} initial="initial" animate="animate">
+
+                        {word}&nbsp;
+                    </motion.span>
+                    )
+                }
+            </motion.h1>
+        
+        </div>
+    )
+}
+
+export default AnimatedText
