0197a95bf711cef1fe78be48a090142d2b82d81b
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2023-05-19 20:16:56 -0300

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -23,13 +23,14 @@ const CustomLink = ({href, title, className=""}) =>{
 
 const NavBar = () =>{
     return(
-        <header className=" w-full px-32 py-8 font-medium flex items-center justify-between">
+        <header className=" w-full px-32 py-8 font-medium grid grid-cols-3 items-center justify-between">
 
             <nav>
                 <CustomLink href="/" title="Home" className="mx-4"/>
                 <CustomLink href="/about" title="About" className="mx-4"/>
                 <CustomLink href="/articles" title="Articles" className="ml-4"/>
             </nav>
+                <Logo/>
             <nav className=" flex items-center justify-center flex-wraps">
 
                 <motion.a href="mailto:joaovitorvianaalves@gmail.com" target={"_blank"} whileHover={{y:-2 }} className="w-8 mr-4">
@@ -45,11 +46,6 @@ const NavBar = () =>{
                 </motion.a>
 
             </nav>
-
-            <div className="absolute left-[50%] top-2
-             translate-x-[50%]">
-                <Logo/>
-            </div>
         </header>
     )
 }
