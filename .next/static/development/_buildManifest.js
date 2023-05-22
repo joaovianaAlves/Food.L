363c80d02d7001ce32671b0ba631d01cb5d8e55e
joaovianaAlves <joaovitorvianaalves@gmail.com>
2023-05-22 18:24:21 -0300

    .

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/fridge":["static\u002Fchunks\u002Fpages\u002Ffridge.js"],sortedPages:["\u002F","\u002F_app","\u002F_erro
