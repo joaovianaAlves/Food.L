8f7f20c9b7d4ef1bec089c00b8086fc93b1f104b
Brenomb <56926880+Brenomb@users.noreply.github.com>
2023-05-24 13:01:59 -0300

    Inserção de dados na tabela de receitas Api.js

--- public/Api.js
+++ public/Api.js
@@ -1,29 +1,32 @@
 export const receitas = [
+
+    //Creme de Milho
     {
         nome: 'Creme de Milho',
         ingredientes: [
             'milho', 'manteiga', 'caldo', 'farinha','leite'
         ],
         ingredientesReais:[
-            '1 lata de milho', '1 colher (sopa) de manteiga', '1 tablete de caldo de legumes', '2 colheres (sopa) de farinha de trigo', '1 e 1/2 xícara (chá) de leite', '1 lata de creme de leite'
+            '1 lata de milho', '1 colher (sopa) de manteiga', '1 tablete de caldo de legumes', '2 colheres (sopa) de farinha de trigo', '1 e 1/2 xícara (chá) de leite', '1 lata de creme de leite', 'salgado'
         ],
-        tags: ['salgado',],
-        modoDePreparo: " 1. No liquidificador, bata o milho e o leite e reserve. \n "
-        + " 2. Em uma panela, derreta a manteiga \n "
-        + " 3. Junte o caldo de legumes, a farinha de trigo e mexa bem. \n "
-        + " 4. Acrescente o milho batido e mexa até engrossar. \n "
-        + " 5. Desligue o fogo e acrescente o creme de leite. "
-        + " 6. Sirva em seguida."
+        modoDePreparo: 
+              " 1. No liquidificador, bata o milho e o leite e reserve. \n "
+            + " 2. Em uma panela, derreta a manteiga \n "
+            + " 3. Junte o caldo de legumes, a farinha de trigo e mexa bem. \n "
+            + " 4. Acrescente o milho batido e mexa até engrossar. \n "
+            + " 5. Desligue o fogo e acrescente o creme de leite. "
+            + " 6. Sirva em seguida."
     },
+
+    //Strognoff de Frango
     {
         nome: 'Strognoff de Frango',
         ingredientes: [
-            'frango', 'creme de leite', 'manteiga'
+            'frango', 'creme de leite', 'manteiga', 'salgado'
         ],
         ingredientesReais: [
             '500g de peito de frango picado em cubos', '1 caixinha de creme de leite', '1 colher (sopa) de ketchup', '1 colher (sopa) de manteiga'
         ],
-        tags: ['Salgado'],
         modoDePreparo: 
               " 1. Cozinhe o peito de frango até ficar bem cozido. \n "
             + " 2. Após o cozimento, adicione o molho de tomate e cozinhe por 5 minutos em fogo baixo. \n "
@@ -31,20 +34,80 @@ export const receitas = [
             + " 4. Por último, adicione o sal a gosto. \n "
             + " 5. Sirva com batata palha e arroz branco à parte. \n "
     },
+
+    //Frivasse de Frango
     {
-        nome: 'Bolo',
+        nome: 'Fricasse de Frango',
         ingredientes: [
-            'farinha', 'nescau', 'ovo', 'fermento'
+            'Fricasse de frango', 'frango', 'creme de leite', 'requeijão', 'batata palha', 'milho verde', 'azeitona', 'mussarela', 'salgado'
         ],
-        tags: ['doce', 'sobremesa'],
-        modoDePreparo: "fazer bolo doce"
+        ingredientesReais: [
+            '2 peitos de frango desfiados', '1 lata de creme de leite', '1 copo de requeijão cremoso', '100g de batata palha', '1 lata de milho verde', '100 g de azeitona sem caroço', '200g de mussarela fatiada', '1 xícara de água', '1 pitada de sal'
+        ],
+        modoDePreparo: 
+              " 1. Bata no liquidificador o milho, o requeijão, o creme de leite e a água. \n "
+            + " 2. Refogue o creme do liquidificador com o frango desfiado, as azeitonas e o sal até ficar com uma textura espessa. \n "
+            + " 3. Coloque o refogado numa assadeira, cubra com mussarela e espalhe a batata palha por cima. \n "
+            + " 4. Leve ao forno até borbulhar. \n "
+            + " 5. Sirva com arroz branco. "
     },
+
+    //Brownie
     {
-        nome: 'Salgado',
+        nome: 'Brownie',
         ingredientes: [
-            'farinha', 'nescau', 'ovo', 'fermento'
+            'achocolatado', 'farinha de trigo', 'açúcar', 'manteiga', 'ovos', 'nescau', 'toddy', 'doce', 'sobremesa'
         ],
-        tags: ['salgado', 'sobremesa'],
-        modoDePreparo: "fazer salgado"
+        ingredientesReais: [
+            '3 xicara de achocolatado', '1 xicara de farinha de trigo', '1 xicara de açúcar', '5 colheres de manteiga', '3 ovos'
+        ],
+        modoDePreparo: 
+              " 1. Derreta a manteiga e reserve \n "
+            + " 2. Enquanto derrete a manteiga, misture os 3 ovos e a açúcar e misture bem \n "
+            + " 3. Acrescente a manteiga derretida no ovo e o açúcar \n "
+            + " 4. Agora é so misturar o achocolatado e a farinha de trigo \n "
+            + " 5. Unte uma forma com manteiga e achocolatado \n "
+            + " 6. Leve ao forno a 180° C por 30 minutos"
+    },
+
+    //Bolo de Cenoura
+    {
+        nome: 'Bolo de Cenoura',
+        ingredientes: [
+            'Cenoura', 'Bolo', 'Bolo de Cenoura', 'doce', 'sobremesa'
+        ],
+        ingredientesReais: [
+              'Massa:','3 cenouras médias raladas', '1/2 xícara (chá) de óleo', '4 ovos', '2 e 1/2 xícaras (chá) de farinha de trigo', '2 xícaras (chá) de açúcar', '1 colher (sopa) de fermento em pó'
+            + 'Cobertura:','1 colher (sopa) de manteiga', '3 colheres (sopa) de chocolate em pó', '1 xícara (chá) de açúcar', 'Uma Xícara de Leite' , 'doce', 'sobremesa'
+        ],  
+        modoDePreparo: 
+              "  MASSA: \n "
+            + " 1. Em um liquidificador, adicione a cenoura, os ovos e o óleo, depois misture. \n "
+            + " 2. Acrescente o açúcar e bata novamente por 5 minutos. \n "
+            + " 3. Em uma tigela ou na batedeira, adicione a farinha de trigo e depois misture novamente. \n "
+            + " 4. Acrescente o fermento e misture lentamente com uma colher. "
+            + " 5. Asse em um forno preaquecido a 180° C por aproximadamente 40 minutos. \n "
+
+            + "  COBERTURA: \n "
+            + " 1. Despeje em uma tigela a manteiga, o chocolate em pó, o açúcar e o leite, depois misture. \n "
+            + " 2. Leve a mistura ao fogo e continue misturando até obter uma consistência cremosa, depois despeje a calda por cima do bolo. "
+    },
+
+    //Escondidinho de Carne
+    {
+        nome: 'Escondidinho de Carne',
+        ingredientes: ['batata', 'carne', 'escondidinho', 'carne moída', 'salgado'],
+        ingredientesReais: [
+            '1 kg de batata', '200 g de queijo mussarela', '1 cebola', 'sal a gosto', 'cheiro-verde a gosto', '2 colheres de manteiga', '500 g de carne moída', 'azeite', '1 dente de alho amassado', 'pimenta branca a gosto', '1/2 copo de leite'
+        ],
+        modoDePreparo: 
+              " 1. Descasque as batatas, corte ao meio e cozinhe com água e sal. \n "
+            + " 2. Depois de cozidas, amasse as batatas, adicione o leite e a manteiga, mexa bem até formar um purê e reserve. \n "
+            + " 3. Em uma panela, adicione 1 fio de azeite, a cebola, o alho e refogue a carne moída. \n "
+            + " 4. Tempere com sal, pimenta branca, cheiro-verde e cozinhe até secar a água que se formar na panela. \n "
+            + " 5. Forre um refratário com a metade do purê de batatas. \n "
+            + " 6. Acrescente uma camada de queijo e uma camada de carne moída. \n "
+            + " 7. Repita as camadas e finalize com o queijo. \n "
+            + " 8. Leve ao forno por 40 minutos. \n "
     },
 ]
